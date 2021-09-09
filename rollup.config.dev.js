fb7b2c748a9b982d7eafd50abaf4cfc4b9808dbb
zhaoxingming <zhaoxingming@nucarf.com>
2021-09-09 10:47:40 +0800

    chore: 预览路径更改

--- rollup.config.dev.js
+++ rollup.config.dev.js
@@ -39,7 +39,8 @@ export default defineConfig({
         dev({
             port: '8888',
             host: 'localhost',
-            spa: './examples/a.html',
+			spa: './examples/index.html',
+			// spa: './examples/a.html',
             force: true,
         }),
         livereload(),
