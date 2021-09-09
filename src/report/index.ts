850a72139d6b9835ff7d37c6f81c33f29cf8e98a
zhaoxingming <zhaoxingming@nucarf.com>
2021-09-09 10:53:36 +0800

    chore: 去掉as断言

--- src/report/index.ts
+++ src/report/index.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: zhaoxingming
  * @Date: 2021-08-24 10:53:10
- * @LastEditTime: 2021-09-09 10:02:15
+ * @LastEditTime: 2021-09-09 10:51:12
  * @LastEditors: vscode
  * @Description: 日志上报方法
  * 1. sendBeacon
@@ -28,7 +28,8 @@ export function clientReport(data: any) {
 
     // return xmlSend(BASE_URL, params);
     // 如果支持sendBeacon 就是用此方法上传日志
-    if ((navigator as any).sendBeacon) return navSendBeacon(BASE_URL, params);
+    if (typeof navigator.sendBeacon === 'function')
+        return navSendBeacon(BASE_URL, params);
     // 兼容各个平台，GET 请求参数不超过 GET_SIZE 字节的大小
     if (params.length < GET_LEN) return imgSend(BASE_URL, params);
     return xmlSend(BASE_URL, params);
