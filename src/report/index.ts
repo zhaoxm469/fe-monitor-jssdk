3ecff24d4074c43fff1a44a393a147b83f4b8353
zhaoxingming <zhaoxingming@nucarf.com>
2021-09-13 15:09:56 +0800

    style: 优化代码结构

--- src/report/index.ts
+++ src/report/index.ts
@@ -1,20 +1,22 @@
 /*
  * @Author: zhaoxingming
  * @Date: 2021-08-24 10:53:10
- * @LastEditTime: 2021-09-10 09:50:42
+ * @LastEditTime: 2021-09-13 15:00:19
  * @LastEditors: vscode
  * @Description: 日志上报
  */
 
-import { BASE_URL, GET_LEN, globalConf } from '../conf/global';
+import { BASE_URL, globalConf } from '../conf/global';
 import { IgnoreArrayType } from '../types';
 import { CommonLog } from '../types/commonLog';
 import { objectTypeName } from '../utils';
 import { setCommonParams } from './common';
 
-import imgSend from './img';
 import navSendBeacon from './sendBeacon';
 import xmlSend from './xmlHttp';
+import imgSend from './img';
+
+const { getMaxLen, ignore, collectionRate } = globalConf;
 
 // 发送请求上报
 export function clientReport(data: CommonLog) {
@@ -25,45 +27,48 @@ export function clientReport(data: CommonLog) {
     // 忽略哪些数据上报
     if (isIgnore(params)) return;
 
-    // 只采集 30% , 降低服务器压力
-    // if (Math.random() < 0.3) return;
+    // 设置采集率，降低服务器压力
+    if (collectionRate > Math.random() * 100) return;
 
-    // 当下几种类型数据传输较少，直接使用img方式进行请求发送
-    const isUseImg =
-        data.level === 'pv' ||
-        data.category === 'pageTime' ||
-        data.category === 'resource';
-
-    if (isUseImg && params.length < GET_LEN) return imgSend(BASE_URL, data);
+    if (isUseImg(data)) return imgSend(BASE_URL, data);
 
     if (typeof navigator.sendBeacon === 'function') {
         return navSendBeacon(BASE_URL, params);
     }
 
-    // 兼容各个平台，GET 请求参数不超过 GET_SIZE 字节的大小
-    if (params.length < GET_LEN) return imgSend(BASE_URL, data);
+    // GET 请求参数不超过 getMaxLen
+    if (params.length < getMaxLen) return imgSend(BASE_URL, data);
+
     return xmlSend(BASE_URL, params);
+}
+
+// 是否使用img方式进行上报
+function isUseImg(data: CommonLog) {
+    // 当下几种类型数据传输较少，直接使用img方式进行请求发送
+    const isUseType =
+        data.level === 'pv' ||
+        data.category === 'pageTime' ||
+        data.category === 'resource';
+
+    return isUseType && JSON.stringify(data).length < getMaxLen;
 }
 
 // 是否忽略当前上报请求
 function isIgnore(params: string): boolean {
-    if (!globalConf.ignore) return false;
+    if (!ignore) return false;
 
-    if (
-        typeof globalConf.ignore === 'string' &&
-        params.includes(globalConf.ignore)
-    ) {
+    if (typeof ignore === 'string' && params.includes(ignore)) {
         return true;
     }
     if (
-        objectTypeName(globalConf.ignore) === 'regexp' &&
-        (globalConf.ignore as RegExp).test(params)
+        objectTypeName(ignore) === 'regexp' &&
+        (ignore as RegExp).test(params)
     ) {
         return true;
     }
 
-    if (objectTypeName(globalConf.ignore) === 'array') {
-        for (let item of globalConf.ignore as IgnoreArrayType) {
+    if (objectTypeName(ignore) === 'array') {
+        for (let item of ignore as IgnoreArrayType) {
             if (typeof item === 'string' && params.includes(item)) {
                 return true;
             }
